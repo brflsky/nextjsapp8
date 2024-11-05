7093ae68afb4d87ad29ecf4bea1fba4ff1283910
Martin Mark <brflsky@gmail.com>
2024-11-05 12:28:53 +0100

    test commit - no to jedziem

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
           priority
         />
         <ol>
+          <li>No to jedziem</li>
           <li>
             Get started by editing <code>src/app/page.js</code>.
           </li>
